2f05a35b16648321f6b80c6bd194eff90b3693f7
danyramirezg <danyramirezg@gmail.com>
2021-03-09 18:47:31 -0500

    Inner classes

--- app/src/main/java/objectOrientedProgramming/InnerClasses.kt
+++ app/src/main/java/objectOrientedProgramming/InnerClasses.kt
@@ -1,2 +1,23 @@
 package objectOrientedProgramming
 
+class Outer1{
+    val myString = "Outside nester class"
+
+    // I have to use the keyword inner to access to the variable of the Outer class
+    inner class NestedClass{
+
+        val newString = "Inside nester class"
+        fun callInside() = "Function call from inside Nested class"
+
+        fun getStuff() = myString
+    }
+}
+
+fun main(args: Array<String>) {
+
+    val outer = Outer1()
+    println("Using Outer object: ${outer.NestedClass().getStuff()}")
+
+    val inner = Outer1().NestedClass() // Using Outer object: Outside nester class
+    println("Using inner object: ${inner.getStuff()}") // Using inner object: Outside nester class
+}
